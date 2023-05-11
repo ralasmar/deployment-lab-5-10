fd2bd50080311521f6b627f943102b5b3935afb1
Rola Alasmar <alasm002@umn.edu>
2023-05-11 15:37:21 -0500

    lab

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const path = require('path')
+const cors = require('cors')
 
 app.use(express.static(`${__dirname}/public`))
+app.use(express.json())
+app.use(cors())
+
+const { ROLLBAR_ACCESS_TOKEN} = process.env
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: `${ROLLBAR_ACCESS_TOKEN}`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+try {
+    app.get('/', fakeFunction => {
+    })
+    .catch (error){
+        rollbar.error(error)
+    }
+}
 
 app.listen(4000, () => console.log('server running on 4000'))
